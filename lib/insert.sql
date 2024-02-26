60ffcb71e24420aab698a58d20ed2b926981b8d2
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-26 19:07:34 +1100

    passed test

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'dark brown', 'calm', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'black', 'aggressive', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'golden', 'gentle', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'brown', 'disciplined', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'light brown', 'playful', TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'green', 'grumpy', FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'white', 'curious', TRUE);
+INSERT INTO bears (id, age, sex, color, temperament, alive) VALUES (NULL, 2, 'F', 'grey', 'shy', TRUE);
